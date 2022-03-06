895fbb8cbd8351fa2ba964f1fb8cc46439823d25
skyoo1226 <you1226@naver.com>
2022-03-06 16:29:13 +0900

    우선 수업시간 따라가면서 작성한 코드

---
+++ app/src/main/java/com/skyoo/orderapp_20220306/adapters/StoreAdapter.kt
@@ -0,0 +1,6 @@
+package com.skyoo.orderapp_20220306.adapters
+
+import android.widget.ArrayAdapter
+
+class StoreAdapter() {
+}
---
+++ app/src/main/java/com/skyoo/orderapp_20220306/datas/StoreData.kt
@@ -0,0 +1,8 @@
+package com.skyoo.orderapp_20220306.datas
+
+class StoreData(
+    val name: String,
+
+
+) {
+}
