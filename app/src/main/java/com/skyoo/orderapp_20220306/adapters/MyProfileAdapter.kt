7a59015167dab5fc0d624221b3f3631aa4e682fe
skyoo1226 <you1226@naver.com>
2022-03-09 05:10:32 +0900

    마이프로필 입력 코드 입력  - 어댑터, 데이터즈, UI 추가 수정

--- app/src/main/java/com/skyoo/orderapp_20220306/adapters/MyProfileAdapter.kt
+++ app/src/main/java/com/skyoo/orderapp_20220306/adapters/MyProfileAdapter.kt
@@ -2,13 +2,12 @@ package com.skyoo.orderapp_20220306.adapters
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import com.skyoo.orderapp_20220306.R
 import kotlinx.android.synthetic.main.fragment_my_profile.*
 import kotlinx.android.synthetic.main.my_profile_list.*
 
-class ProfileAdapter : AppCompatActivity() {
+class MyProfileAdapter : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.my_profile_list)
@@ -16,8 +15,8 @@ class ProfileAdapter : AppCompatActivity() {
         btnSave.setOnClickListener {
             val  inputNickname = btnEditNickname.text.toString()
             val  resultintent = Intent()
-            resultintent.putExtra("nick", inputNickname)
 
+            resultintent.putExtra("nick", inputNickname)
             setResult(RESULT_OK, resultintent)
 
             finish()
